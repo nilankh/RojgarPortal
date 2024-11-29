dfc6fd92745295cb71fe63405f2b5a53eb9f8d00
nilank nikhil <nilanknikhil@gmail.com>
2024-11-29 15:51:05 +0530

    job page

--- app/Http/Controllers/JobsController.php
+++ app/Http/Controllers/JobsController.php
@@ -2,12 +2,25 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Category;
+use App\Models\Job;
+use App\Models\JobType;
 use Illuminate\Http\Request;
 
 class JobsController extends Controller
 {
     //job page
     public function index(){
-        return view('front.jobs');
+
+        $categories = Category::where('status', 1)->get();
+        $jobTypes = JobType::where('status', 1)->get();
+        // to use relation we need to use with
+        $jobs = Job::where('status', 1)->with('jobType')->orderBy('created_at','DESC')->paginate(9);
+        
+        return view('front.jobs', [
+            'categories'=>$categories,
+            'jobTypes'=>$jobTypes,
+            'jobs'=>$jobs,
+        ]);
     }
 }
